74d18aa083f5bccaf34af46b05d479e599888a11
LionyxML <rahul.juliato@gmail.com>
2021-12-05 01:20:11 -0300

    The experiment

--- redux-basic-hooks/src/App.js
+++ redux-basic-hooks/src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { incrementA, selectA } from "./index";
 
 function App() {
+  // The hooks way
+  const dispatch = useDispatch();
+  const incA = () => dispatch({ type: "INCREMENT_A" });
+  const incB = () => dispatch({ type: "INCREMENT_B" });
+  const zeroAll = () => dispatch({ type: "ZERO_ALL" });
+
+  const aaa = useSelector((state) => state.aaa);
+  const bbb = useSelector((state) => state.bbb);
+
+  // The "another" way
+  const aValue = selectA();
+
   return (
     <div className="App">
-      <div> This app!</div>
+      The hooks way:
+      <div>
+        The aaa value is: {aaa} (<span onClick={incA}>+</span>)
+      </div>
+      <div>
+        The bbb value is: {bbb} (<span onClick={incB}>+</span>)
+      </div>
+      <div onClick={zeroAll}>Everything is zeroed</div>
+      <hr width="200px" />
+      <div>
+        Another way:
+        <div>
+          The aaa value is: {aValue} (<span onClick={incrementA}>+</span>)
+        </div>
+      </div>
     </div>
   );
 }
--- redux-basic-hooks/src/index.js
+++ redux-basic-hooks/src/index.js
@@ -2,10 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+// REDUX: Initial State
+const initialState = {
+  aaa: 1000,
+  bbb: 2000,
+};
+// REDUX: Root Reducer
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT_A":
+      return {
+        ...state,
+        aaa: state.aaa + 1,
+      };
+    case "INCREMENT_B":
+      return {
+        ...state,
+        bbb: state.aaa + 1,
+      };
+    case "ZERO_ALL":
+      return {
+        aaa: 0,
+        bbb: 0,
+      };
+    default:
+      return state;
+  }
+}
+
+// REDUX: Store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// REDUX: Selectors
+export const selectA = () => store.getState().aaa;
+
+// REDUX: Dispatchers
+export const incrementA = () => store.dispatch({ type: "INCREMENT_A" });
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
